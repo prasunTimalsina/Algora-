ceea4b4eba2eed6504ce3f7b1400203d56784e85
Prasun Timalsina <timalsinaprasun61@gmail.com>
2025-05-19 14:10:13 +0545

    Complete basic problem Controller and Routes

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -20,7 +20,7 @@ export const createProblem = asyncHandler(async (req, res) => {
     codeSnippets,
     referenceSolutions,
   } = req.body;
-  console.log(Object.entries(referenceSolutions));
+
   for (const [language, code] of Object.entries(referenceSolutions)) {
     console.log(language, code);
     const languageId = getJudge0LanguageId(language);
@@ -82,3 +82,134 @@ export const createProblem = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(201, newProblem, "Problem created successfully"));
 });
+
+export const getAllProblem = asyncHandler(async (req, res) => {
+  const problems = await db.problem.findMany();
+  if (!problems) {
+    throw new ApiError(400, "Error fetching Problems");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, problems, "Problems Fetched successfully"));
+});
+
+export const getProblemById = asyncHandler(async (req, res) => {
+  const { problemId } = req.params;
+
+  const problem = await db.problem.findUnique({
+    where: {
+      id: problemId,
+    },
+  });
+
+  if (!problem) {
+    throw new ApiError(404, "Problem not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, problem, "Fetched Problem sucessfully"));
+});
+
+export const deleteProblem = asyncHandler(async (req, res) => {
+  const { problemId } = req.params;
+
+  const problem = await db.problem.delete({
+    where: {
+      id: problemId,
+    },
+  });
+
+  if (!problem) {
+    throw new ApiError(404, "Problem not found");
+  }
+
+  res.status(200).json(new ApiResponse(204, null, "Problem deleted"));
+});
+
+export const updateProblem = asyncHandler(async (req, res) => {
+  const { problemId } = req.params;
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+  let { testcases } = req.body;
+
+  if (testcases || referenceSolutions) {
+    if (!testcases) {
+      const problem = await db.problem.findUnique({ where: { id: problemId } });
+      testcases = problem.testcases;
+    }
+
+    for (const [language, code] of Object.entries(referenceSolutions)) {
+      const languageId = getJudge0LanguageId(language);
+
+      if (!languageId) {
+        throw new ApiError(400, "Language not supported");
+      }
+
+      const submissions = testcases.map(({ input, output }) => ({
+        source_code: code,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+
+      const submissionResults = await submitBatch(submissions);
+
+      const tokens = submissionResults.map((res) => res.token);
+
+      const results = await pollBatchResults(tokens);
+
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        console.log("Result____", result);
+
+        if (result.status.id !== 3) {
+          return res
+            .status(400)
+            .json(
+              new ApiError(
+                400,
+                `Test ${i + 1} cases failed for language ${language} `
+              )
+            );
+        }
+      }
+    }
+  }
+
+  const problem = await db.problem.update({
+    where: {
+      id: problemId,
+    },
+    data: {
+      title,
+      description,
+      difficulty,
+      tags,
+      examples,
+      constraints,
+      testcases,
+      codeSnippets,
+      referenceSolutions,
+    },
+  });
+
+  if (!problem) {
+    throw new ApiError(404, "Problem not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, problem, "Problem updated successfully"));
+});
+
+export const getAllProblemSolvedByUser = asyncHandler(async (req, res) => {});
--- backend/src/routes/problem.routes.js
+++ backend/src/routes/problem.routes.js
@@ -1,9 +1,20 @@
 import express from "express";
 
 import { isAdmin, verifyJwt } from "../middleware/auth.middleware.js";
-import { createProblem } from "../controllers/problem.controller.js";
+import {
+  createProblem,
+  deleteProblem,
+  getAllProblem,
+  getProblemById,
+  updateProblem,
+} from "../controllers/problem.controller.js";
 const problemRouter = express.Router();
 
 problemRouter.route("/create-problem").post(verifyJwt, isAdmin, createProblem);
-
+problemRouter.route("/get-all-problem").get(verifyJwt, getAllProblem);
+problemRouter.route("/get-problem/:problemId").get(verifyJwt, getProblemById);
+problemRouter
+  .route("/delete-problem/:problemId")
+  .delete(verifyJwt, isAdmin, deleteProblem);
+problemRouter.route("/update-problem/:problemId").put(verifyJwt, isAdmin,updateProblem);
 export default problemRouter;
